d21e957c64896e9ec86483c534ed7d79d844c70b
grwna <13523035@std.stei.itb.ac.id>
2025-06-22 16:12:15 +0700

    feat: implement utility functions

--- src/text_processor.py
+++ src/text_processor.py
@@ -6,18 +6,35 @@ def clean_text(text: str) -> str:
             newtext += char
     return newtext
 
+
 # takes a matrix of text, then decrypt a certain column, TEXTS ARE UPPERCASE
-def caesar_decrypt_columns(texts: list[str], col_idx : int, shift : int) -> None:
-    for i in range(len(texts)):
-        row = list(texts[i])
-        row[col_idx] = chr(((ord(row[col_idx]) - 65) + shift % 26) + 65)
-        texts[i] = ''.join(row)
+def caesar_decrypt(text: list, shift : int) -> str:
+    result = ""
+    for char in text:
+        result += chr(((ord(char) - 65 - shift) % 26) + 65)
+    return result
 
 
+def create_text_matrix(text: str, row_len: int) -> list[list[str]]:
+    text_only_alphabets = ''.join(char for char in text if char.isalpha())
+    return [list(text_only_alphabets[i:i+row_len]) for i in range(0, len(text_only_alphabets), row_len)]
 
+
+def get_column(matrix : list[list[str]], column : int) -> str:
+    column_chars = [row[column] for row in matrix if column < len(row)]
+    return "".join(column_chars)
+
+
+# turns newkeynewkeynewkey into newkey
+def get_shortest_non_repeating_sequence(text: str) -> str:
+    n = len(text)
+    for length in range(1, n+1):
+        prefix = text[:length]
+        if n % length == 0:
+            if (prefix*(n//length) == text):
+                return prefix
+    return text
+
+    
 if __name__ == "__main__":
     print(clean_text(" aksjdaiwuj!8* ad ?.;\";"))
-
-    lis = ["abc", "bcd", "cde", "def", "efg"]
-    caesar_decrypt_columns(lis, 1, 2)
-    print(lis)
